5c581dd0297808688d7bd67cff2b2b16d64c41a4
Kyle Gran <kgran@gmail.com>
2023-09-02 15:21:06 -0700

    created initial files and worked on some of the JS.

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const questions = require('./lib/questions');
+const shapes = require('./lib/shapes');
+
+
