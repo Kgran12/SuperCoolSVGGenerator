f3584d7255d16c9bca9541138d65c971dd37ec29
Kyle Gran <kgran@gmail.com>
2023-09-07 16:29:49 -0700

    created tests, wrapped up questions.

--- index.js
+++ index.js
@@ -5,3 +5,35 @@ const questions = require('./lib/questions');
 const shapes = require('./lib/shapes');
 
 
+const generateFile = (svgFile) => {
+    let filePath = path.resolve(`${__dirname}/new_logo_file`, 'logo.svg')
+    fs.writeFile(filePath, svgFile, err => {
+        if (err) throw err;
+        console.log('Generated logo.svg')
+    })
+}
+
+const runInq = () => {
+    inquirer.prompt(
+        questions.array 
+    )
+    .then((answers) => {
+        generateFile(
+            shapes.generateNewShape(answers)
+        ); 
+
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+          console.log('Can\'t be rendered');
+        } else {
+          console.log('Something went wrong');
+        }
+      })
+}
+
+let init = () => {
+    runInq();
+}
+
+init ();
