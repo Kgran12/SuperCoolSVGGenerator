f24af791d39c6ec16c248bc478076ab2617030bb
Kyle Gran <kgran@gmail.com>
2023-11-20 11:56:32 -0700

    created readme and wrapped up code.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # SuperCoolSVGGenerator
+
+## Table of Contents
+- Description
+- Usage
+- Features
+- Walkthrough
+- Questions
+
+## Description
+
+This application is a Node.js  command-line app that will create a SVG logo for your project. This allows you to customize your logos shapes, colors,and text!
+
+## Usage
+
+To use this app please follow these steps:
+
+- Open the terminal in the root folder and enter node.index.js
+- Answer the prompts with the information regarding your logo.
+- Once all the prompts have been answered, your SVG logo will be saved inyour project directory.
+
+## Walkthrough
+
+
+
+## Credits
+
+GitHub: Kgran12
+
+for any questions, contact kgran12675@gmail.com
--- index.js
+++ index.js
@@ -1,39 +1,58 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const path = require('path');
-const questions = require('./lib/questions');
-const shapes = require('./lib/shapes');
+const { Circle, Triangle, Square } = require('./lib/shapes');
 
 
-const generateFile = (svgFile) => {
-    let filePath = path.resolve(`${__dirname}/new_logo_file`, 'logo.svg')
-    fs.writeFile(filePath, svgFile, err => {
-        if (err) throw err;
-        console.log('Generated logo.svg')
-    })
-}
+async function userInput () {
+    const usersChoice = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'What shape would you like to draw?',
+            choices: ['Circle', 'Triangle', 'Square']
+        },
+        {
+            type: 'input',
+            name: 'color',
+            message: 'What color would you like your shape to be?'
+        },
+        {
+            type: 'input',
+            name: 'text',
+            message: 'What text would you like your shape to have?'
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'What color would you like your text to be?'
+        },
+       
+    ]);
 
-const runInq = () => {
-    inquirer.prompt(
-        questions.array 
-    )
-    .then((answers) => {
-        generateFile(
-            shapes.generateNewShape(answers)
-        ); 
+    let selectedShape;
+    switch (usersChoice.shape) {
+        case 'Circle':
+            selectedShape = new Circle();
+            break;
+        case 'Triangle':
+            selectedShape = new Triangle();
+            break;
+        case 'Square':
+            selectedShape = new Square();
+            break;
+    }
 
-    })
-    .catch((error) => {
-        if (error.isTtyError) {
-          console.log('Can\'t be rendered');
-        } else {
-          console.log('Something went wrong');
+    selectedShape.setColor(usersChoice.color);
+    selectedShape.setText(usersChoice.text);
+    selectedShape.setTextColor(usersChoice.textColor);
+
+    const svg = selectedShape.render();
+
+    fs.writeFile('output.svg', svg, (err) => {
+        if (err) {
+            console.log(err);
         }
-      })
+    });
 }
 
-let init = () => {
-    runInq();
-}
-
-init ();
+userInput();
